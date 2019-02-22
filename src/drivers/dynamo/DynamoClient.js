20248671e49074bbb9ad4c6fdf0274f88e3b08c5
kohei <andokohei@gmail.com>
2019-02-22 12:15:50 +0900

    initial commit

---
+++ src/drivers/dynamo/DynamoClient.js
@@ -0,0 +1,106 @@
+const AWS = require('aws-sdk');
+import config from 'config';
+const p = console.log;
+AWS.config.logger = console;
+
+AWS.config.update({
+  sslEnabled: false,
+  accessKeyId: 'test',
+  secretAccessKey: 'test',
+  region: 'us-east-1',
+  endpoint: 'dynamodb:8000'
+});
+
+const db = new AWS.DynamoDB();
+
+/**
+ * put a single object
+ * @param { string } table_name
+ * @param { object } item
+ */
+async function putItem(table_name, item) {
+  try {
+    const params = {
+      TableName: table_name,
+      Item: item
+    };
+    return await db.putItem(params).promise();
+  } catch (error) {
+    throw new DynamoError(error);
+  }
+}
+
+/**
+ * get by the key
+ * @param { string } table_name
+ * @param { object } key : schema { 'KEYNAME': { DATA_YPE : 'VALUE' } }
+ * @param { string } projection_exp *optional
+ */
+async function getItem(table_name, key, projection_exp) {
+  try {
+    let params = {
+      TableName: table_name,
+      Key: key
+    };
+    if (projection_exp) params.ProjectionExpression = projection_exp;
+    const data = await db.getItem(params).promise();
+    return data.Item;
+  } catch (error) {
+    p(error);
+    throw new DynamoError(error);
+  }
+}
+
+/**
+ * @param { obj } params
+ */
+async function queryTable(params) {
+  try {
+    const data = await db.query(params).promise();
+    return data.Items;
+  } catch (error) {
+    throw new DynamoError(error);
+  }
+}
+
+/**
+ * @param { obj } params
+ */
+async function scanTable(params) {
+  try {
+    const data = await db.scan(params).promise();
+    return data.Items;
+  } catch (error) {
+    throw new DynamoError(error);
+  }
+}
+
+/**
+ * delete by the key
+ * @param { string } table_name
+ * @param { object } key : schema { 'KEYNAME': { DATA_YPE : 'VALUE' } }
+ */
+async function deleteItem(table_name, key) {
+  try {
+    const params = {
+      TableName: table_name,
+      Key: key
+    };
+    return await db.deleteItem(params).promise();
+  } catch (error) {
+    throw new DynamoError(error);
+  }
+}
+
+/**
+ * MySql2 module error class
+ */
+class DynamoError extends Error {
+  constructor(error_object) {
+    super(error_object.message);
+    this.message = error_object.message;
+    this.statusCode = 500;
+  }
+}
+
+export default { putItem, getItem, queryTable, scanTable, deleteItem };
